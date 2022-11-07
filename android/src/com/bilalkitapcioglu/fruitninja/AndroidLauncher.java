d07f7c9325afb99d11cc21530b6c04be2ce3c3b2
Bilal Kitapçıoğlu <bilalkitapcioglu@gmail.com>
2022-11-07 22:28:47 +0300

    Files upload

---
+++ android/src/com/bilalkitapcioglu/fruitninja/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bilalkitapcioglu.fruitninja;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bilalkitapcioglu.fruitninja.FruitNinja;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FruitNinja(), config);
+	}
+}
